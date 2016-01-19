d1ef06b26dd496f68fab17ecb973b0bcbf427025
Ben Ebel <bge1234@gmail.com>
2016-01-19 15:56:21 -0700

    finished

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,4 +3,32 @@ var router = express.Router();
 
 /* Movies Routes Go Here. */
 
+router.get('/:movie_id/reviews', function (req, res, next) {
+  res.render('movies/show');
+});
+
+router.get('/:movie_id/reviews/new', function (req, res, next) {
+  res.render('movies/reviews/new');
+});
+
+router.post('/:movie_id/reviews', function (req, res, next) {
+  res.redirect('/movies/show');
+});
+
+router.get('/:movie_id/reviews/:id', function (req, res, next) {
+  res.render('movies/reviews/show');
+});
+
+router.get('/:movie_id/reviews/:id/edit', function (req, res, next) {
+  res.render('movies/reviews/edit');
+});
+
+router.post('/:movie_id/reviews/:id', function (req, res, next) {
+  res.redirect('/movies/show');
+});
+
+router.post('/:movie_id/reviews/:id/delete', function (req, res, next) {
+  res.redirect('/movies/show');
+});
+
 module.exports = router;
