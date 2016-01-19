7e9305f2ab2564e7f7b436c3f0a863fa7c102f7f
Ben Ebel <bge1234@gmail.com>
2016-01-19 15:37:33 -0700

    finished

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/movies', movies);
-app.use('/reviews', reviews);
+app.use('/movies', reviews);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,32 +3,32 @@ var router = express.Router();
 
 /* Movies Routes Go Here. */
 
-// router.get('/', function (req, res, next) {
-//   res.render('movies/index');
-// });
-//
-// router.get('/new', function (req, res, next) {
-//   res.render('movies/new');
-// });
-//
-// router.post('/', function (req, res, next) {
-//   res.redirect('/');
-// });
-//
-// router.get('/:id', function (req, res, next) {
-//   res.render('movies/show');
-// });
-//
-// router.get('/:id/edit', function (req, res, next) {
-//   res.render('movies/edit');
-// });
-//
-// router.post('/:id', function (req, res, next) {
-//   res.redirect('/');
-// });
-//
-// router.post('/:id/delete', function (req, res, next) {
-//   res.redirect('/');
-// });
+router.get('/:movie_id/reviews', function (req, res, next) {
+  res.render('movies/show');
+});
+
+router.get('/:movie_id/reviews/new', function (req, res, next) {
+  res.render('movies/reviews/new');
+});
+
+router.post('/:movie_id/reviews', function (req, res, next) {
+  res.redirect('/movies/show');
+});
+
+router.get('/:movie_id/reviews/:id', function (req, res, next) {
+  res.render('movies/reviews/show');
+});
+
+router.get('/:movie_id/reviews/:id/edit', function (req, res, next) {
+  res.render('movies/reviews/edit');
+});
+
+router.post('/:movie_id/reviews/:id', function (req, res, next) {
+  res.redirect('/movies/reviews');
+});
+
+router.post('/:movie_id/reviews/:id/delete', function (req, res, next) {
+  res.redirect('/movies/reviews');
+});
 
 module.exports = router;
